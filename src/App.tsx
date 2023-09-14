a399c96076b3ac3243e8622e92636e4ec0f1157c
Steve Magana <steveamagana@gmail.com>
2023-09-14 02:24:42 -0700

    Remove unnecessary import and fix styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route} from 'react-router-dom'
-import { Container } from 'react-bootstrap'
 import { Home } from './pages/Home'
 import { Store } from './pages/Store'
 import { About } from './pages/About'
